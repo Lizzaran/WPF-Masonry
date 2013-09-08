5c3f0da571c507580316a59e1a1c3be37ac148d2
Nikita Bernthaler <lizzaran92@gmail.com>
2013-09-08 22:14:36 +0200

    Initial release

---
+++ MasonryLibrary/Class/EventArgs.cs
@@ -0,0 +1,38 @@
+﻿using System.Windows;
+
+namespace MasonryLibrary
+{
+    internal class EventArgs
+    {
+        #region Nested type: PositionChanged
+
+        internal class PositionChanged : EventArgs
+        {
+            public PositionChanged(Tile.Position position)
+            {
+                Position = position;
+            }
+
+            public Tile.Position Position { get; private set; }
+        }
+
+        #endregion
+
+        #region Nested type: SizeChanged
+
+        internal class SizeChanged : EventArgs
+        {
+            public SizeChanged(Size previousSize, Size newSize)
+            {
+                PreviousSize = previousSize;
+                NewSize = newSize;
+            }
+
+            public Size PreviousSize { get; private set; }
+
+            public Size NewSize { get; private set; }
+        }
+
+        #endregion
+    }
+}
